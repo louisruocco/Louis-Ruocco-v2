c4eef7cda5a64e0efa054c0d3fde380e1bd7a201
Louis Ruocco <louisruocco1@gmail.com>
2022-01-07 13:55:34 +0000

    landing page done

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const projectBoxes = document.querySelectorAll(".project");
-const portfolio = document.querySelector(".projects");
+const grids = document.querySelectorAll(".background-div");
+const links = document.querySelectorAll(".links");
+const nav = document.querySelector(".nav");
 
-function scrollAppear(element, top){
-    const topPosition = element.getBoundingClientRect().top
-    const windowPosition = window.innerHeight / top;
+let num = 0;
 
-    if(topPosition < windowPosition){
-        element.classList.add("appear")
-    }
-}
-
-function slideIn(title, text, top){
-    const topPosition = title.getBoundingClientRect().top
-    const windowPosition = window.innerHeight / top;
-
-    if(topPosition < windowPosition){
-        title.classList.add("slide");
-        text.classList.add("slide-other");
-    }
-}
-
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".nav");
-    const aboutTitle = document.querySelector(".about-title");
-    const aboutText = document.querySelector(".about-text");
-    const portfolioTitle = document.querySelector(".portfolio-title");
-    const portfolioText = document.querySelector(".portfolio-text");
-    const projects = document.querySelector(".projects");
-
-    scrollAppear(nav, 1);
-    scrollAppear(projects, 2);
-
-    slideIn(aboutTitle, aboutText, 2.5)
-    slideIn(portfolioTitle, portfolioText, 2.5)
+grids.forEach(grid => {
+    setTimeout(() => {
+        grid.style.backgroundColor = "black";
+    }, num += 100)
 });
 
-projectBoxes.forEach(box => { 
-    box.addEventListener("mouseover", (e) => {
-        if(e.target.classList == "project"){
-            const text = e.target.children[1];
-            text.classList.add("appear");
-        }
-    })
+links.forEach(link => {
+    setTimeout(() => {
+        nav.classList.add("appear");
+        link.classList.add("appear");
+    }, num += 500)
+})
+
+function pushDownAppear (element, top){
+    const topPosition = element.getBoundingClientRect().top;
+    const windowPosition = window.innerHeight / top;
+    if(topPosition < windowPosition){
+        element.classList.add("push-down-appear");
+    }
+}
+
+function slideIn (element, top){
+    const topPosition = element.getBoundingClientRect().top;
+    const windowPosition = window.innerHeight / top;
+    if(topPosition < windowPosition){
+        element.classList.add("slide-in");
+    }
+}
+
+window.addEventListener("load", () => {
+    const title = document.querySelector(".title");
+    const subtitle = document.querySelector(".subtitle");
+    setTimeout(() => {
+        pushDownAppear(title, 0);
+    }, 3500)
+    setTimeout(() => {
+        slideIn(subtitle, 0);
+    }, 4500)
 })
